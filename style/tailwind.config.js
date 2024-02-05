ae9f8ef45d41167f8ce260e44e91681f7c928343
amine.chbari.etu <amine@DESKTOP-QK66G6C>
2024-02-05 04:12:50 +0100

    publishing my portfolio

---
+++ style/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  darkModeClass: 'dark-mode',
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
